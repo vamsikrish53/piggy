fd45fe445e6ead039c643429136212fb509efa83
ADITYA DASAMANTHARAO <dasamantarao.aditya@gmail.com>
2019-07-03 23:15:53 +0530

    Simple spring boot runnable app

---
+++ PIGGY-Backend/src/main/java/piggy/springbootstarter/PiggyApp.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package piggy.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ACER
+ *
+ */
+@SpringBootApplication
+public class PiggyApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(PiggyApp.class, args);
+
+	}
+
+}
